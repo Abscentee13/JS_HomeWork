1a4d69ee8d8c79e62d6358d7941f139bd3ee5de6
NSviehaper <sviehaper@gmail.com>
2022-12-02 14:56:25 +0200

    HomeWork06 additional JS End

--- Homework_JS_06 additional/scripts/main.js
+++ Homework_JS_06 additional/scripts/main.js
@@ -1,18 +1,18 @@
 // - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
 // document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
 
-function cutString (str, n)
-{
-    let sliceArray = [];
-    while (str.length > 0) {
-        sliceArray.push(str.substring(0, n));
-        str = str.substring(n, str.length);
+    function cutString (str, n)
+    {
+        let sliceArray = [];
+        while (str.length > 0) {
+            sliceArray.push(str.substring(0, n));
+            str = str.substring(n, str.length);
+        }
+        return sliceArray;
     }
-    return sliceArray;
-}
-let str = "неочікуваний";
-let n=5;
-console.log(cutString(str, n));
+    let str = "неочікуваний";
+    let n = 5;
+    console.log(cutString(str, n));
 
 
 // - Напишіть функцію delete_characters(str, length), яка повертає підрядок,
@@ -21,13 +21,13 @@ str = 'Каждый охотник желает знать';
 // document.writeln(delete_characters(str, 7)); // Каждый
 //
 
-function delete_characters(str, length)
-{
-    return str.substring(0, length);
-}
+    function delete_characters(str, length)
+    {
+        return str.substring(0, length);
+    }
 
-let lengthPartOfString=5;
-console.log(delete_characters(str, lengthPartOfString));
+    let lengthPartOfString=5;
+    console.log(delete_characters(str, lengthPartOfString));
 
 
 
@@ -37,22 +37,23 @@ console.log(delete_characters(str, lengthPartOfString));
 // document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
 //
 
-function insert_dash(str)
-{
-  return str.toUpperCase()
-            .replaceAll(" ", "-");
-}
+    function insert_dash(str)
+        {
+          return str.toUpperCase()
+                    .replaceAll(" ", "-");
+        }
 
-console.log(insert_dash(str));
+    console.log(insert_dash(str));
 
 // - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр
 // першого символу рядка з нижнього регістру у верхній.
 //
-str = "первый символ ";
-function upper_first_letter(str) {
-    return (str[0].toUpperCase() + str.substring(1, str.length - 1));
-}
-console.log(upper_first_letter(str));
+    str = "первый символ ";
+    function upper_first_letter(str)
+            {
+                return (str[0].toUpperCase() + str.substring(1, str.length - 1));
+            }
+    console.log(upper_first_letter(str));
 
 // - Дано список імен.
 let n1 = 'Harry..Potter'
@@ -65,71 +66,68 @@ let n3 = 'Hermione__Granger'
 // let n3 = 'Hermione Granger'
 //
 
-console.log(validationName(n1));
-console.log(validationName(n2));
-console.log(validationName(n3));
+    console.log(validationName(n1));
+    console.log(validationName(n2));
+    console.log(validationName(n3));
 
-function validationName (noValidName)
-{
-    for (let i=0; i<noValidName.length; i++)
+    function validationName (noValidName)
     {
-        if (!(noValidName.toLowerCase().charCodeAt(i) >= 97 && noValidName.toLowerCase().charCodeAt(i) <=122))
+        for (let i=0; i<noValidName.length; i++)
         {
-            noValidName = noValidName.replace(noValidName[i], " ")
+            if (!(noValidName.toLowerCase().charCodeAt(i) >= 97 && noValidName.toLowerCase().charCodeAt(i) <=122))
+            {
+                noValidName = noValidName.replace(noValidName[i], " ")
+            }
         }
+        while (noValidName !== noValidName.replace("  ", " "))
+        {
+            noValidName = noValidName.replace("  ", " ");
+        }
+        return noValidName;
     }
-    while (noValidName !== noValidName.replace("  ", " "))
-    {
-        noValidName = noValidName.replace("  ", " ");
-    }
-    return noValidName;
-}
 
 
 
 //     - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
 
-n = 10;
-
-numberArray = (n => {
-    let numberArray = [];
-    for (let i=0; i < n; i++)
-        {
-            numberArray.push(Math.round(Math.random() * 100));
-        }
-    return numberArray;
-
-});
+    numberArray = (n => {
+        let numberArray = [];
+        for (let i=0; i < n; i++)
+            {
+                numberArray.push(Math.round(Math.random() * 100));
+            }
+        return numberArray;
+    });
 
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень.
 // Відсортувати його за допомоги sort
 
-console.log(numberArray(14).sort(function (num1, num2)
-{
-    if (num1 > num2) return 1;
-    if (num1 < num2) return -1;
-    if (num1 === num2) return 0;
-}));
+    console.log(numberArray(14).sort(function (num1, num2)
+    {
+        if (num1 > num2) return 1;
+        if (num1 < num2) return -1;
+        if (num1 === num2) return 0;
+    }));
 
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень.
 // відфільтрувати  його за допомоги filter, залишивши тільки парні числа (без 0!)
 //
 
-console.log(numberArray(14).filter(num1 => num1 % 2 === 0 && num1 !== 0));
+    console.log(numberArray(14).filter(num1 => num1 % 2 === 0 && num1 !== 0));
 
 // - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
-str = "первый символ ";
+    str = "первый символ ";
 
-function capitalize(str) {
-    let tempArrayString = [];
-    str.split(' ').forEach(function (tmpString)
-    {
-        tempArrayString.push(tmpString.substring(0, 1).toUpperCase() + tmpString.substring(1));
-    });
-    return tempArrayString.join(' ');
-    };
+    function capitalize(str) {
+        let tempArrayString = [];
+        str.split(' ').forEach(function (tmpString)
+        {
+            tempArrayString.push(tmpString.substring(0, 1).toUpperCase() + tmpString.substring(1));
+        });
+        return tempArrayString.join(' ');
+        }
 
-console.log(capitalize(str));
+    console.log(capitalize(str));
 
 
 // - Створити функцію-валідатор для адрес електронної пошти.
@@ -146,31 +144,37 @@ console.log(capitalize(str));
 // Для тих, хто дуже розумний, та почне використовувати регулярні вирази одразу "ні". Своїм мозком подумайте над протоколом, з регулярками будете потім бавитись.
 //
 
-let mailString = "someemail@gmail.com";
+        let mailString;
 
-function mailValidation(mailString) {
-        return !((mailString.split('@').length !== 2) ||
-            (mailString.split('@')[0] === "") ||
-            (mailString.split('@')[1] === "") ||
-            (mailString.split('@')[1].split('.').length < 2) ||
-            (mailString.split('@')[1].split('.')[mailString.split('@')[1].split('.').length-1].length < 2) ||
-            (mailString.split('@')[1].split('.')[0].length < 1))
-        };
-mailString = "someemail@gmail.com";
-console.log(mailString + " valid is " + mailValidation(mailString));
-mailString = "some.email@gmail.com";
-console.log(mailString + " valid is " + mailValidation(mailString));
-mailString = "someeMAIL@i.ua";
-console.log(mailString + " valid is " + mailValidation(mailString));
-mailString = "someeMAIL@i.ua";
-console.log(mailString + " valid is " + mailValidation(mailString));
-mailString = "someemail@gmail.comsomee@mail@gmail.com";
-console.log(mailString + " valid is " + mailValidation(mailString));
-mailString = "someeMAIL@.ua";
-console.log(mailString + " valid is " + mailValidation(mailString));
-mailString = "someemaomsomeegmail.com";
-console.log(mailString + " valid is " + mailValidation(mailString));
+        function mailValidation(mailString) {
+                return !((mailString.split('@').length !== 2) ||
+                    (mailString.split('@')[0] === "") ||
+                    (mailString.split('@')[1] === "") ||
+                    (mailString.split('@')[1].split('.').length < 2) ||
+                    (mailString.split('@')[1].split('.')[mailString.split('@')[1].split('.').length-1].length < 2) ||
+                    (mailString.split('@')[1].split('.')[0].length < 1))
+                }
 
+        mailString = "someemail@gmail.com";
+        console.log(mailString + " valid is " + mailValidation(mailString));
+
+        mailString = "some.email@gmail.com";
+        console.log(mailString + " valid is " + mailValidation(mailString));
+
+        mailString = "someeMAIL@i.ua";
+        console.log(mailString + " valid is " + mailValidation(mailString));
+
+        mailString = "someeMAIL@i.ua";
+        console.log(mailString + " valid is " + mailValidation(mailString));
+
+        mailString = "someemail@gmail.comsomee@mail@gmail.com";
+        console.log(mailString + " valid is " + mailValidation(mailString));
+
+        mailString = "someeMAIL@.ua";
+        console.log(mailString + " valid is " + mailValidation(mailString));
+
+        mailString = "someemaomsomeegmail.com";
+        console.log(mailString + " valid is " + mailValidation(mailString));
 
 // - є масив
 let coursesArray = [
@@ -271,7 +275,7 @@ let coursesArray = [
             count++;
         }
         return count;
-    };
+    }
 
 
 // - Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів.
@@ -290,7 +294,7 @@ let coursesArray = [
                 tempArrayString.push(str.split(' ')[i]);
             }
         return tempArrayString.join(' ');
-    };
+    }
 
 // -стоврити масив книжок (назва, кількість сторінок, автори , жанри).
 
@@ -298,9 +302,10 @@ let book = [
 
     {name:`foreword`, pageCount: 14, authors: ['AAA', 'BBB', 'CCC'], genre: ['ccc']},
     {name:`boyWhoLived`, pageCount: 18, authors: ['AAA'], genre: ['aaa', 'ccc']},
+    {name:`boyWhoLived full`, pageCount: 18, authors: ['AAA'], genre: ['aaa', 'ccc']},
     {name:`vanishingGlass`, pageCount: 13, authors: ['AAA', 'BBB', ], genre: ['aaa', 'bbb', 'ccc', 'ddd']},
     {name:`lettersFromNoOne`, pageCount: 17, authors: ['BBB', 'CCC'], genre: ['aaa']},
-    {name:`afterword`, pageCount: 19, authors: ['CCC'], genre: ['aaa', 'bbb', 'ccc']}
+    {name:`afterword`, pageCount: 19, authors: ['CCC'], genre: ['aaa', 'bbb', 'ccc', 'fff']}
 
 ];
 console.log(book);
@@ -320,42 +325,87 @@ console.log(book.sort(function (book1, book2)
 
 console.log('знайти книжку/ки з найбільшою кількістю жанрів ');
 
-let books = [
 
-    {name:`foreword`, pageCount: 14, authors: ['AAA', 'BBB', 'CCC'], genre: ['ccc']},
-    {name:`boyWhoLived`, pageCount: 18, authors: ['AAA'], genre: ['aaa', 'ccc']},
-    {name:`vanishingGlass`, pageCount: 13, authors: ['AAA', 'BBB', ], genre: ['aaa', 'bbb', 'ccc', 'ddd']},
-    {name:`lettersFromNoOne`, pageCount: 17, authors: ['BBB', 'CCC'], genre: ['aaa']},
-    {name:`afterword`, pageCount: 19, authors: ['CCC'], genre: ['aaa', 'bbb', 'ccc']}
+let maxGenre = (books) =>
+    {
+        let maxCount =  books.sort(function (book1, book2)
+                                        {
+                                             if (book1.genre.length > book2.genre.length) return -1;
+                                             if (book1.genre.length < book2.genre.length) return 1;
+                                             if (book1.genre.length === book2.genre.length) return 0;
+                                        })
+                        [0].genre.length;
 
-];
+        books = books.reduce((bookWithMax, book) =>
+                            {
+                                if (book.genre.length === maxCount)
+                                {
+                                     bookWithMax.push(book);
+                                }
+                                return bookWithMax;
+                            }
+                , []);
+        return books;
+    };
 
-let maxGenre = (books) => {
-    books.sort(function (book1, book2) {
-                    if (book1.genre.length > book2.genre.length) return -1;
-                    if (book1.genre.length < book2.genre.length) return 1;
-                    if (book1.genre.length === book2.genre.length) return 0;
-                });
-
-    return books[0].name;
-};
-
-console.log(maxGenre(books));
+console.log(maxGenre(book));
 
 // - знайти книжку/ки з найдовшою назвою
 
-console.log('знайти найбільшу книжку ');
-console.log(book.sort(function (book1, book2)
-{
-    if (book1.name.length > book2.name.length) return -1;
-    if (book1.name.length < book2.name.length) return 1;
-    if (book1.name.length === book2.name.length) return 0;
-})[0]);
+    console.log('знайти книжку/ки з найдовшою назвою ');
+
+    let maxName = (books) =>
+    {
+        let maxCount =  books.sort(function (book1, book2)
+        {
+            if (book1.name.length > book2.name.length) return -1;
+            if (book1.name.length < book2.name.length) return 1;
+            if (book1.name.length === book2.name.length) return 0;
+        })
+            [0].name.length;
+
+        books = books.reduce((bookWithMax, book) =>
+            {
+                if (book.name.length === maxCount)
+                {
+                    bookWithMax.push(book);
+                }
+                return bookWithMax;
+            }
+            , []);
+        return books;
+    };
+
+    console.log(maxName(book));
 
 // - знайти книжку/ки які писали 2 автори
 
+console.log('знайти книжку/ки які писали 2 автори ');
+
+let twoAuthors =  book.reduce((bookWithMax, book) =>
+    {
+        if (book.authors.length === 2)
+        {
+            bookWithMax.push(book);
+        }
+        return bookWithMax;
+    }
+    , []);
+console.log(twoAuthors);
 
 // - знайти книжку/ки які писав 1 автор
+console.log('знайти книжку/ки які писали 1 автор ');
+
+let oneAuthors =  book.reduce((bookWithMax, book) =>
+    {
+        if (book.authors.length === 1)
+        {
+            bookWithMax.push(book);
+        }
+        return bookWithMax;
+    }
+    , []);
+console.log(oneAuthors);
 
 
 // - вісортувати книжки по кількості сторінок по зростанню
