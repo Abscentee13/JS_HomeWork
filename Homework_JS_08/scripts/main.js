8ba39a62396afd80b0de67bf0801c5e0d6889f37
NSviehaper <sviehaper@gmail.com>
2022-12-09 11:39:12 +0200

    HomeWork08 Continue

--- Homework_JS_08/scripts/main.js
+++ Homework_JS_08/scripts/main.js
@@ -84,26 +84,37 @@ console.log(clients.sort(function (client1, client2) {
     if (client1.order.length === client2.order.length) return 0;
 }));
 
+
+///////////////////////////////////////////////////////////////
 // - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску,
 // максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
 
-
 // -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
-
-
-
 // -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
-
-
-
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
-
 
 
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 
 
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+function Car(carModel, carManufacturer, carYear, carMaxSpeed, carEngineVolume) {
+    this.carModel = carModel;
+    this.carManufacturer = carManufacturer;
+    this.carYear = carYear;
+    this.carMaxSpeed = carMaxSpeed;
+    this.carEngineVolume = carEngineVolume;
+    this.drive = function(){
+        console.log(`їдемо зі швидкістю ${this.carMaxSpeed} на годину`)
+    }
+    this.info = function (){
+        console.log(` carModel - ${this.carModel} \n carManufacturer - ${this.carManufacturer} \n carYear - ${this.carYear} \n carMaxSpeed - ${this.carMaxSpeed} \n carEngineVolume - ${this.carEngineVolume}`);
+    }
+}
+
+let newCar = new Car('Audi', 'Audi', '2010', '210', '2,4');
+newCar.drive();
+newCar.info();
 
 // - (Те саме, тільки через клас)
 
